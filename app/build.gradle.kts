e4dd7c3d7c90eddaf6957002e70bb807e5ce2b7e
rcmiku <rcmiku@gmail.com>
2025-02-08 12:39:30 +0800

    app: Bump version to 1.1
    Signed-off-by: rcmiku <rcmiku@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.rcmiku.ncm.dumper"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 11
+        versionName = "1.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
